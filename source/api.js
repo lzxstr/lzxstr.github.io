91152ff00730ce2515f5d1001abb5bf6aa6d472f
lzxstr <96021554+lzxstr@users.noreply.github.com>
2022-02-26 16:42:17 +0100

    update

--- source/api.js
+++ source/api.js
@@ -2,6 +2,18 @@ function NotAva () {
     alert ("Not available yet.");
 }
 
+function krnl () {
+    window.location = "https://krnl.ca";
+}
+
 function Download1 () {
     window.location = "https://link-target.net/414440/roblox-beam-tool-download";
+}
+
+function Download2 () {
+    window.location = "https://link-hub.net/414440/krnl-executor-download";
+}
+
+function Download3 () {
+    window.location = "https://link-center.net/414440/roblox-fps-unlocker";
 }
